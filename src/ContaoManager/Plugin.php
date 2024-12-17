b650c31722ba937346ba29b7b7176b0cac536399
birds in the sun <73081731+birdsinthesun@users.noreply.github.com>
2024-12-17 21:18:42 +0100

    Update Plugin.php

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -18,7 +18,7 @@ class Plugin implements BundlePluginInterface,RoutingPluginInterface
     {
         return [
             BundleConfig::create(IsoProductfeedBundle::class)
-                ->setLoadAfter([ContaoCoreBundle::class],'isotope'])
+                ->setLoadAfter([ContaoCoreBundle::class,'isotope'])
                 
                
         ];
